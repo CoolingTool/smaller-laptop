9c352876a02dd91b98084bd2ed305cf545c2e50e
CoolingTool <62089272+CoolingTool@users.noreply.github.com>
2025-03-01 23:30:01 -0600

    i read the docs this time

--- files/scripts/mutter-patched.sh
+++
@@ -1,3 +0,0 @@
-cd /etc/yum.repos.d/
-wget  https://copr.fedorainfracloud.org/coprs/trixieua/mutter-patched/repo/fedora-$(rpm -E %fedora)/trixieua-mutter-patched-fedora-$(rpm -E %fedora).repo
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland gdm
